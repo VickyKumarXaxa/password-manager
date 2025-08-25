e92f6d95a356a3dbfba8570a3267298d62d6cdf2
Vicky Kumar <vickykumar@Vickys-MacBook-Air.local>
2025-08-25 11:08:26 +0530

    Added Table TSX component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function HomePage(){
-  return <div>Home Page</div>
+import { Table } from "@/components/Table";
+import { SAVED_PASSWORD_DISPLAY_TABLE_HEADERS } from "@/utility/constants";
+
+export default function HomePage() {
+  const tableData = [["Google", "PasswordManager", "Password", "Delete"]];
+
+  return (
+    <div>
+      Home Page
+      <Table
+        tableHeaders={SAVED_PASSWORD_DISPLAY_TABLE_HEADERS}
+        tableData={tableData}
+      />
+    </div>
+  );
 }
